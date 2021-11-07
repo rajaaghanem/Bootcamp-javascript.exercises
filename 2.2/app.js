653903022e3034b09d23880cb481f408fcb71ab2
rajaaghanem <rajaaghanem@gmail.com>
2021-11-07 15:07:54 +0200

    2.2

---
+++ 2.2/app.js
@@ -0,0 +1,8 @@
+function tellFortune(jobTitle, location, partnerName, numberOfChildren){
+
+    let str= `You will be a ${jobTitle} in ${location} and married to ${partnerName} with ${numberOfChildren} children.`;
+    return str;
+
+}
+
+console.log(str=tellFortune("teacher", "Haifa", "Aviv", 2));
