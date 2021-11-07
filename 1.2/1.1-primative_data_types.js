8803ec6d417eb888cd51094eaa059b06fb0026be
rajaaghanem <rajaaghanem@gmail.com>
2021-11-07 14:36:46 +0200

    1.2

---
+++ 1.2/1.1-primative_data_types.js
@@ -0,0 +1,67 @@
+/*
+--Delete the wrong answers--
+
+1. Which of the following is/are strings?
+
+
+c) '4'
+
+2. Which of the following is/are numbers?
+
+a) 4
+b) 4.0
+d) -4
+
+3. Which of the following is/are booleans?
+
+a) true
+b) false
+
+
+4. What is the result of 80 + 71.2?
+
+a) 151.2
+
+
+5. What is the result of "80" + 71.2?
+
+
+c) "8071.2"
+
+6. Does 100 + 30 produce a number or a string?
+
+a) number
+
+7. Does "100" + 30 produce a number or a string?
+
+b)string
+
+*/
+
+//------Submit your answers under the question------//
+
+//create three different ways to declare variables
+let num =1;
+var num1=2;
+const num2 =3;
+
+//declare a variable and reassign a value to it
+let name="rajaa";
+
+
+//create a variable and after that assign a string with its value being: He's got it!
+
+let str = " he\'s got it!";
+
+/*
+1. create a variable and assign a value on how much a restaurant bill is.
+let bill = 30;
+2. create a variable and assign a value on how much tax they should pay.
+let tax= 0.2;
+3. create a variable that will calculate the bill * tax and its output would be: Your total bill is <total bill> $.
+let total = `your total bill is ${bill + bill*tax};` */
+
+// Round the number 50.6 to its nearest integer
+Math.round(50.6);
+//Create a variable that is undefined
+let num;
---
+++ 1.2/app.js
@@ -0,0 +1,7 @@
+console.log(80 + 71.2);
+let bill = 30;
+let tax= 0.2;
+let total = `the tptal bill is : ${bill + bill*tax}`;
+let str= "moo";
+let num;
+console.log(num);
