b1abe7bb0adc77d5fafa61672b4fb30b48679e55
rajaaghanem <rajaaghanem@gmail.com>
2021-11-07 16:58:58 +0200

    date

--- 2.4/app.js
+++ 2.4/app.js
@@ -1,11 +1,11 @@
 function currDate(){
     let str= new Date();
-    months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
-    weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+    months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+    weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
     str=`today is ${weekday[str.getDay()]} the ${str.getDate()} of ${months[str.getMonth()]
     }, ${str.getFullYear()}`;
 
-    return str.toString();
+    return str;
 }
 
 console.log(date=currDate());
