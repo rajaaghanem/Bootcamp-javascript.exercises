90f66efbc2c83084784d55f28a0c4fb9becedbf2
rajaaghanem <rajaaghanem@gmail.com>
2021-11-07 15:47:47 +0200

    2.4

---
+++ 2.4/app.js
@@ -0,0 +1,11 @@
+function currDate(){
+    let str= new Date();
+    months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+    weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+    str=`today is ${weekday[str.getDay()]} the ${str.getDate()} of ${months[str.getMonth()]
+    }, ${str.getFullYear()}`;
+
+    return str.toString();
+}
+
+console.log(date=currDate());
