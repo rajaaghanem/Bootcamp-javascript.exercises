e34ad97a582310a545c78c719340013771e4b681
rajaaghanem <rajaaghanem@gmail.com>
2021-11-07 14:14:34 +0200

    ex

--- 1.1-primitive_data_types.js
+++ 1.1-primitive_data_types.js
@@ -1,1 +1,8 @@
-let me=1;
+let num = 99;
+let str="rajaa";
+let bol=true;
+let und= 0/0;
+let nu=null;
+let car = {type:"Fiat", model:"500", color:"white"};
+let sym2 = Symbol('hello');
+console.log(str);
