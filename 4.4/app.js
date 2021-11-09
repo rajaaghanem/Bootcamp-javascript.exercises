44ade762461ec19b9a410da9663287cc8f7ebb7e
rajaaghanem <rajaaghanem@gmail.com>
2021-11-09 09:33:15 +0200

    ..

--- 4.4/app.js
+++ 4.4/app.js
@@ -1,12 +1,15 @@
 function div7(num){
+    let numbersArr=[];
     for (let i=1; i<=num; i++){
         let str=String(i);
-        if (str.includes('7') && (i%7 === 0)) console.log("BOOM-BOOM");
-        else if (i%7 === 0) console.log("BOOM");
-        else console.log(i);
-
+        if (str.includes('7') && (i%7 === 0)) 
+        numbersArr.push("BOOM-BOOM");
+        else if (i%7 === 0) 
+        numbersArr.push("BOOM");
+        else  numbersArr.push(i);
     }
+   console.log(numbersArr.toString()) ;
 }
 
 
-div7(18);
+div7(70);
