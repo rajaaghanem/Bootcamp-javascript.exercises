1f243e1c931fd55dba92989e3c6b2d9ae3d7685a
rajaaghanem <rajaaghanem@gmail.com>
2021-11-11 09:16:48 +0200

    8.2

--- 8.2/app.js
+++ 8.2/app.js
@@ -7,7 +7,10 @@ const mycountry ={
     describe() {
         console.log(`${this.country} has ${this.population} million people, their mother tongue is ${this.language}, they
         have ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}`);
+    },
+    isIsland: function checkIsland(){
+        return mycountry.neighbours.length? true:false;
     }
 };
-
 mycountry.describe();
+console.log(mycountry.isIsland());
