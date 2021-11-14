13a138ea6ceaa17b57b78caa9ac802911db01fa0
rajaaghanem <rajaaghanem@gmail.com>
2021-11-14 11:21:26 +0200

    9.2

---
+++ loops/9.2_loop.js
@@ -0,0 +1,24 @@
+function  populationPercentages(population){
+    let i = 0;
+    let percentages=[];
+
+    while (i<4){
+        let res = percentageOfWorld1(population[i]);
+        percentages.push(res+ "%");
+        i++;
+    }
+    return percentages;
+}
+
+
+function percentageOfWorld1(population){
+    const worldPop= 7900000000;
+    let res = (population/worldPop)*100;
+    return res;
+}
+
+
+//! test
+let population = [9000000, 1200000, 400099,30000000];
+let result= populationPercentages(population);
+console.log(result);
