b0e7b7549ca16b64ea655b71160d6b5d52fe86fa
rajaaghanem <rajaaghanem@gmail.com>
2021-11-15 17:49:33 +0200

    12.2

---
+++ find_and_filter/12.2.js
@@ -0,0 +1,53 @@
+const candyStore = {
+    candies: [
+    {
+    name: "mint gum",
+    id: "as12f",
+    price: 2,
+    amount: 2
+    },
+    {
+    name: "twix",
+    id: "5hd7y",
+    price: 5,
+    amount: 4
+    },
+    ],
+    cashRegister: 200
+   }
+
+   function getCandy(candyStore, id){
+    const obj=candyStore.candies.find(candy=> candy.id===id);
+    return obj;
+    } 
+
+    // console.log(getCandy(candyStore,"5hd7y"));
+
+    function getPrice(candyStore, id){
+    return getCandy(candyStore, id).price;
+    } 
+    
+    function addCandy(candyStore, id, name, price){
+        let obj={
+            id : id,
+            name : name,
+            price :price,
+            amount: 1
+        }
+        candyStore.candies.push(obj);
+    }
+
+    function buy(candyStore, id){
+        candyStore.cashRegister = candyStore.cashRegister + getPrice(candyStore, id);
+        getCandy(candyStore, id).amount --;
+
+    }
+
+
+    console.log(getCandy(candyStore,"5hd7y"));
+    console.log(getPrice(candyStore,"5hd7y"));
+    addCandy(candyStore, "444", "wow", 2)
+    buy(candyStore, "444");
+    console.log(candyStore);
+
+
