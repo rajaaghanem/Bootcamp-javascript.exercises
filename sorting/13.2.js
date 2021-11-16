2d7c73acc414eed76a29a94d56b6710ad4a58551
rajaaghanem <rajaaghanem@gmail.com>
2021-11-16 11:11:42 +0200

    13.2

--- sorting/13.2.js
+++ sorting/13.2.js
@@ -1,3 +1,5 @@
+// 1
+
 const foods = ["falafel", "sabich", "hummus", "pizza with extra pineapple"];
 
 foods.sort();
@@ -5,3 +7,48 @@ console.log(foods);
 
 foods.reverse();
 console.log(foods);
+
+
+// 2
+
+const foodsWithUpperCase = [
+    "falafel",
+    "Sabich",
+    "hummus",
+    "pizza with extra pineapple",
+   ];
+
+   // ascending
+   foodsWithUpperCase.sort((a,b)=> {
+       a=a.toLowerCase();
+       b=b.toLowerCase();
+       if (a<b) return -1;
+       if (a>b) return 1;
+       if (a===b) return 0;
+    });
+
+    // desending
+    foodsWithUpperCase.sort((a,b)=> {
+        a=a.toLowerCase();
+        b=b.toLowerCase();
+        if (a>b) return -1;
+        if (a<b) return 1;
+        if (a===b) return 0;
+     });
+ 
+   console.log(foodsWithUpperCase);
+
+   // 3
+   
+   const words = ["apple", "supercalifragilisticexpialidocious", "hi", "zoo"];
+
+   words.sort((a,b)=> {
+       if (a.length < b.length) return 1;
+       if (a.length > b.length) return -1;
+       if (a.length === b.length) return 0;
+   })
+
+
+   console.log(words);
+
+   
