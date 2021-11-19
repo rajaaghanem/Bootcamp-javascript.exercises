343fc8ea3e1b66bde24defadfb313c997ceb640a
rajaaghanem <rajaaghanem@gmail.com>
2021-11-19 09:48:04 +0200

    17.1

---
+++ scope _ hoisting/scope.js
@@ -0,0 +1,25 @@
+
+
+// movie = "me and you";
+
+
+function outer(){
+    // let movie = "lord of the rings";
+    console.log(movie);
+
+    function inner(){
+        // let movie = "up";
+        console.log(movie);
+
+        function extraInner(){
+            // let movie = "fight club";
+            console.log(movie);
+
+        }
+        extraInner();
+
+    }
+    inner();
+}
+
+outer();
