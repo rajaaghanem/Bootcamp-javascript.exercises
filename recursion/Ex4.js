9cfc73c674c6484da91742044571f38cc7b67abc
rajaaghanem <rajaaghanem@gmail.com>
2021-11-21 15:35:44 +0200

    ..

--- recursion/Ex4.js
+++ recursion/Ex4.js
@@ -1,8 +1,6 @@
 function pow(exponent, number){
   return  (exponent===0)? 1: pow(exponent-1, number)*number;
-    // if (exponent===0)
-    // return 1;
-    // return pow(exponent-1, number)*number;
+
 }
 
 console.log(pow(2,4));
